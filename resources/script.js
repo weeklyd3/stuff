99c80d9b28bf31805ea09a630f4d376667851435
weeklyd3 <79176077+weeklyd3@users.noreply.github.com>
2021-03-01 22:53:31 -0800

    Update script.js

--- resources/script.js
+++ resources/script.js
@@ -2,6 +2,23 @@
 *** download
 *** Font size (view)
 */
+$('textarea').keydown(function(e) {
+  var keyCode = e.keyCode || e.which;
+
+  if (keyCode === 9) {
+    e.preventDefault();
+    var start = this.selectionStart;
+    var end = this.selectionEnd;
+
+    // set textarea value to: text before caret + tab + text after caret
+    spaces = "    ";
+    this.value = this.value.substring(0, start) + spaces + this.value.substring(end);
+
+    // put caret at right position again
+    this.selectionStart = this.selectionEnd = start + spaces.length;
+  }
+});
+
 function PrintElem(elem)
 {
     var mywindow = window.open('', 'PRINT', 'height=400,width=600');
