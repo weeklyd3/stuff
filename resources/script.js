bb459b4da72938d0b71c434bab32b831353e384b
weeklyd3 <79176077+weeklyd3@users.noreply.github.com>
2021-04-07 17:25:43 -0700

    Added FOUR SPACES in textarea when "--> Tab" is pressed

--- resources/script.js
+++ resources/script.js
@@ -16,6 +16,18 @@ function getEmbedSelection(iframe) {
   }
 }
 
+var textareas = document.getElementsByTagName('textarea');
+var count = textareas.length;
+for(var i=0;i<count;i++){
+    textareas[i].onkeydown = function(e){
+        if(e.keyCode==9 || e.which==9){
+            e.preventDefault();
+            var s = this.selectionStart;
+            this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
+            this.selectionEnd = s+1; 
+        }
+    }
+}
 
 function PrintElem(elem) {
     var mywindow = window.open('', 'PRINT', 'height=400,width=600');
